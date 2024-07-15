54aa36991eda8abe89c123f3bac191baed172e40
JV26vasr <veronicaro3101qr@gmail.com>
2024-07-15 13:48:05 -0600

    agregando codigo en js de pantalla

---
+++ controllers/admin/pantalla.js
@@ -0,0 +1,79 @@
+// Constante para completar la ruta de la API.
+const LIBRO_API =  'services/admin/libros.php';
+
+// Método del evento para cuando el documento ha cargado.
+document.addEventListener('DOMContentLoaded', () => {
+    // Constante para obtener el número de horas.
+    const HOUR = new Date().getHours();
+    // Se define una variable para guardar un saludo.
+    let greeting = '';
+    // Dependiendo del número de horas transcurridas en el día, se asigna un saludo para el usuario.
+    if (HOUR < 12) {
+        greeting = 'Buenos días';
+    } else if (HOUR < 19) {
+        greeting = 'Buenas tardes';
+    } else if (HOUR <= 23) {
+        greeting = 'Buenas noches';
+    }
+    // Llamada a la función para mostrar el encabezado y pie del documento.
+    loadTemplate();
+    // Se establece el título del contenido principal.
+    MAIN_TITLE.textContent = `${greeting}, bienvenido`;
+    // Llamada a la funciones que generan los gráficos en la página web.
+    graficoBarrasCategorias();
+    graficoPastelEditoriales();
+});
+
+/*
+*   Función asíncrona para mostrar un gráfico de barras con la cantidad de productos por categoría.
+*   Parámetros: ninguno.
+*   Retorno: ninguno.
+*/
+const graficoBarrasCategorias = async () => {
+    // Petición para obtener los datos del gráfico.
+    const DATA = await fetchData(PRODUCTO_API, 'cantidadProductosCategoria');
+    // Se comprueba si la respuesta es satisfactoria, de lo contrario se remueve la etiqueta canvas.
+    if (DATA.status) {
+        // Se declaran los arreglos para guardar los datos a graficar.
+        let categorias = [];
+        let cantidades = [];
+        // Se recorre el conjunto de registros fila por fila a través del objeto row.
+        DATA.dataset.forEach(row => {
+            // Se agregan los datos a los arreglos.
+            categorias.push(row.nombre_categoria);
+            cantidades.push(row.cantidad);
+        });
+        // Llamada a la función para generar y mostrar un gráfico de barras. Se encuentra en el archivo components.js
+        barGraph('chart1', categorias, cantidades, 'Cantidad de productos', 'Cantidad de productos por categoría');
+    } else {
+        document.getElementById('chart1').remove();
+        console.log(DATA.error);
+    }
+}
+
+/*
+*   Función asíncrona para mostrar un gráfico de pastel con el porcentaje de productos por categoría.
+*   Parámetros: ninguno.
+*   Retorno: ninguno.
+*/
+const graficoPastelEditoriales = async () => {
+    // Petición para obtener los datos del gráfico.
+    const DATA = await fetchData(LIBRO_API, 'porcentajeProductosEditorial');
+    // Se comprueba si la respuesta es satisfactoria, de lo contrario se remueve la etiqueta canvas.
+    if (DATA.status) {
+        // Se declaran los arreglos para guardar los datos a gráficar.
+        let editoriales = [];
+        let porcentajes = [];
+        // Se recorre el conjunto de registros fila por fila a través del objeto row.
+        DATA.dataset.forEach(row => {
+            // Se agregan los datos a los arreglos.
+            editoriales.push(row.nombre);
+            porcentajes.push(row.porcentaje);
+        });
+        // Llamada a la función para generar y mostrar un gráfico de pastel. Se encuentra en el archivo components.js
+        pieGraph('chart2', editoriales, porcentajes, 'Porcentaje de productos por editorial');
+    } else {
+        document.getElementById('chart2').remove();
+        console.log(DATA.error);
+    }
+}
