07f1e99e563d92772da8d24b578f0053e6f40216
JV26vasr <veronicaro3101qr@gmail.com>
2024-08-13 07:37:21 -0600

    correcciones de grafica por editorial en la consulta y su titulo

--- api/models/handler/libro_handler.php
+++ api/models/handler/libro_handler.php
@@ -235,7 +235,8 @@ class LibroHandler
         $sql = 'SELECT nombre, ROUND((COUNT(id_libro) * 100.0 / (SELECT COUNT(id_libro) FROM tb_libros)), 2) porcentaje
                 FROM tb_libros
                 INNER JOIN tb_editoriales USING(id_editorial)
-                GROUP BY nombre ORDER BY porcentaje DESC';
+                GROUP BY nombre ORDER BY porcentaje DESC
+                LIMIT 5';
         return Database::getRows($sql);
     }
 
